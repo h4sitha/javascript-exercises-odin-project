2559acfb80f0680289b4162ffffe096dc2fe3ad8
Hasitha Ekanayake <150508298+h4sitha@users.noreply.github.com>
2024-02-16 14:16:50 +0530

    Finish completing the fourth test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let newArr = [];
+    array.forEach(checkAll);
+    function checkAll(num) {
+        if (!args.includes(num)){
+            newArr.push(num);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
