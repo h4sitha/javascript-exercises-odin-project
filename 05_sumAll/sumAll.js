c4082c2b31a88600cbd09e7517b1673e506096ec
Hasitha Ekanayake <150508298+h4sitha@users.noreply.github.com>
2024-02-16 14:37:41 +0530

    Finish completing the fifth test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (num1 > 0 && num2 > 0 && typeof num1 ===
+        "number" && typeof num2 === "number") {
+        let lowNum = Math.min(num1, num2);
+        let highNum = Math.max(num1, num2);
+
+        let sum = 0;
+
+        for(let i=lowNum; i <= highNum; i++) {
+            sum += i;
+        }
+
+        return sum;
+    } else {
+        return "ERROR";
+    }
 };
+
+// get two numbers
+// add them and the numbers in between them to get the sum
+
+// first find which is lowest and highest
+// assign i to lowest and run until i equals to highest
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
