26abc59864446e703bb107f487b90e539cb2aeeb
Hasitha Ekanayake <150508298+h4sitha@users.noreply.github.com>
2024-02-16 20:38:24 +0530

    Finish completing the sixth test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year%400 !== 0 && year%100 === 0) {
+        return false;
+    } else if (year%4 === 0){
+        return true;
+    } else {
+        return false;
+    }
 };
+
+// divisible by 4: true
+// divisible by 100 and not 400: false
+// % to find a remained for this
 
 // Do not edit below this line
 module.exports = leapYears;
