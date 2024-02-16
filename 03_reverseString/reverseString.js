73026c418741a2f0d5295f63f4af93996b2df0cb
Hasitha Ekanayake <150508298+h4sitha@users.noreply.github.com>
2024-02-16 12:41:23 +0530

    Finish completing the third test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let length = word.length;
+    let letters = word.split("");
+    let string = "";
+    for (let i=length-1; i >= 0; i--) {
+        string += letters[i];
+    }
+    return string;
 };
 
 // Do not edit below this line
