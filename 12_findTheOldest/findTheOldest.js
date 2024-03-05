883bd0f30d945c79dadb5c3cf4c7929b21b1f16e
Hasitha Ekanayake <150508298+h4sitha@users.noreply.github.com>
2024-03-05 20:47:48 +0530

    Redo the exercise using reduce method

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,20 +1,37 @@
 const findTheOldest = function(arr) {
 
-    arrCopy = Array.from(arr);
+    // arrCopy = Array.from(arr);
 
-    for (let person of arrCopy) {
-        if (!person.yearOfDeath) {
-            person.yearOfDeath = getAge();
+    // for (let person of arrCopy) {
+    //     if (!person.yearOfDeath) {
+    //         person.yearOfDeath = getAge();
+    //     }
+    // }
+
+    // arrCopy.sort((a, b) => {
+    //     return (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth);
+    // });
+    // return arrCopy[(arrCopy.length-1)];
+
+    // function getAge() {
+    //     return new Date().getFullYear();
+    // }
+
+    return arr.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        const currentAge = getAge(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
+        if (oldestAge < currentAge) {
+            return currentPerson;
+        } else {
+            return oldest;
         }
-    }
+    })
 
-    arrCopy.sort((a, b) => {
-        return (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth);
-    });
-    return arrCopy[(arrCopy.length-1)];
-
-    function getAge() {
-        return new Date().getFullYear();
+    function getAge (death, birth) {
+        if (!death) {
+            death = new Date().getFullYear();
+        }
+        return death - birth;
     }
 };
 
