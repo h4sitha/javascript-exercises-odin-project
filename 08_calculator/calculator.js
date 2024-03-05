fbc775fd637fb694a7a972a5ae9c1ac9937d48a7
Hasitha Ekanayake <150508298+h4sitha@users.noreply.github.com>
2024-03-05 16:16:01 +0530

    Finish js object exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, curr) => {
+    return sum + curr
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, curr) => {
+    return product * curr
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  num = a;
+	for (let i=1; i < b; i++) {
+    num *= a;
+  }
+  return num;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) {
+    return 1;
+  }
+  let num = 1;
+	for (let i=a; i > 0; i--) {
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
