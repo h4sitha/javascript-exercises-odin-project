abd2cf4aa61f1855d62ec917673a794908230afb
Hasitha Ekanayake <150508298+h4sitha@users.noreply.github.com>
2024-03-05 20:13:58 +0530

    Finish the 12th exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+    arrCopy = Array.from(arr);
+
+    for (let person of arrCopy) {
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = getAge();
+        }
+    }
+
+    arrCopy.sort((a, b) => {
+        return (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth);
+    });
+    return arrCopy[(arrCopy.length-1)];
+
+    function getAge() {
+        return new Date().getFullYear();
+    }
 };
 
 // Do not edit below this line
